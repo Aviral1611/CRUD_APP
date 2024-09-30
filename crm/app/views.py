4a0ff28a85d9138574d61843454a2d49145c92bf
Aviral <aviral.vbansal16@gmail.com>
2024-09-30 23:14:14 +0530

    Added navbar and made register page

--- crm/app/views.py
+++ crm/app/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+
+from .forms import CreateUserForm, LoginForm
 
 # Create your views here.
 
@@ -8,3 +10,22 @@ def home(request):
     # return HttpResponse("Hello, world. You're at the home page.")
 
     return render(request,'app/index.html')
+
+#- Register a User
+
+
+
+def register(request):
+    form = CreateUserForm()
+
+    if request.method == "POST":
+        form = CreateUserForm(request.POST)
+
+        if form.is_valid():
+            form.save()
+
+            # return redirect('')
+
+    context = {'form':form}
+
+    return render(request, 'app/register.html',context=context)
